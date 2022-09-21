111de5d31692b85efface37c7503e0b16d7bf6a3
dogukant <dogukantokgoz4@gmail.com>
2022-09-21 07:20:20 +0300

    asd

--- iletisim.php
+++ iletisim.php
@@ -12,7 +12,7 @@
         <section class="page-section" id="contact">
             <div class="container">
                 <div class="text-center">
-                    <h2 class="section-heading text-uppercase mt-3"><?php echo $sonuc ["baslik"]; ?></h2>
+                    <h2 class="section-heading text-uppercase mt-3"><?php echo $sonuc ["baslik"]; ?></h2>saddsa
                     <h3 class="section-subheading text-muted"><?php echo $sonuc ["aciklama"]; ?></h3>
                 </div>
                 <form id="contactForm" name="sentMessage" novalidate="novalidate">
